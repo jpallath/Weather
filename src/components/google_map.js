b4080046087222ffce315d3dd17bcaf563a515d6
Jerry Pallath <pallathj@gmail.com>
2017-09-17 20:06:12 -0400

    implemented GoogleMaps api

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component{
   componentDidMount() {
-    new google.map.Map(this.refs.map, {
+    new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
@@ -11,9 +11,9 @@ class GoogleMap extends Component{
     });
   }
 
-  render (
+  render () {
     return <div ref="map" />;
-  )
+};
 }
 
 export default GoogleMap;
